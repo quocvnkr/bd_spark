6ccfc4e2dd090e5d9b6c24f36b6a5cde71f10ee1
Giau Ngo <1707627+7hbyh17fUqS5uA@users.noreply.github.com>
2022-06-09 17:51:41 -0500

    step3

---
+++ src/main/java/bd/Main.scala
@@ -0,0 +1,30 @@
+package bd
+
+import org.apache.spark.sql.functions.{col, mean, round, variance}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Main extends App {
+  val conf = new SparkConf().setAppName("Spark and SparkSql").setMaster("local")
+  val sc = new SparkContext(conf)
+  sc.setLogLevel("WARN")
+  val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+  import sqlContext.implicits._
+
+  val csv = sc.textFile("mtcars.csv")
+  val headerAndRows = csv.map(line => line.split(",").map(_.trim))
+  val header = headerAndRows.first
+  val mtcdata = headerAndRows.filter(_(0) != header(0))
+
+  val populationDF = mtcdata
+    .map(p =>  (p(2), p(1)))
+    .toDF("cyl", "mpg")
+  //populationDF.printSchema
+  //populationDF.show()
+
+  populationDF
+    .groupBy(col("cyl").as("Category"))
+    .agg(round(mean("mpg"), 2).as("Mean"),
+      round(variance("mpg"), 2).as("Var"))
+    .sort("Category")
+    .show()
+}
