61d6dee2861cf8158289d0ce34b04796b2d580d9
Giau Ngo <1707627+7hbyh17fUqS5uA@users.noreply.github.com>
2022-06-09 18:06:56 -0500

    prj

--- src/main/java/bd/Main.scala
+++ src/main/java/bd/Main.scala
@@ -4,6 +4,7 @@ import org.apache.spark.sql.functions.{col, mean, round, variance}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Main extends App {
+  // step 1
   val conf = new SparkConf().setAppName("Spark and SparkSql").setMaster("local")
   val sc = new SparkContext(conf)
   sc.setLogLevel("WARN")
@@ -15,16 +16,23 @@ object Main extends App {
   val header = headerAndRows.first
   val mtcdata = headerAndRows.filter(_(0) != header(0))
 
+  // step 2
   val populationDF = mtcdata
     .map(p =>  (p(2), p(1)))
     .toDF("cyl", "mpg")
-  //populationDF.printSchema
-  //populationDF.show()
 
+  // step 3
   populationDF
     .groupBy(col("cyl").as("Category"))
     .agg(round(mean("mpg"), 2).as("Mean"),
       round(variance("mpg"), 2).as("Var"))
     .sort("Category")
     .show()
+
+  // step 4
+  val sampleDF = populationDF.sample(false, 0.25)
+  sampleDF.show()
+
+  // step 5
+  val resampleData = sampleDF.sample
 }
